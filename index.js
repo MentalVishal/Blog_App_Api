9ef8aea06a8ae436611795ceedba897fe1757edc
vishal kumar <vishalkumar36565@gmail.com>
2023-09-28 18:36:05 +0530

    eval

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const { Connection } = require("./db");
 const { userRouter } = require("./Routes/userRoutes");
 const { blogRouter } = require("./Routes/blogRouter");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("", userRouter);
 app.use("/blogs", blogRouter);
